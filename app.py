fee4ce0356d29be4c95808deffb45892fd4a107f
Nguyen Le Bao Long <baolong1112001@gmail.com>
2026-03-11 11:08:21 +0800

    Fix formatting of description in app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ st.set_page_config(page_title="Will Tools", layout="wide")
 
 st.title("Will Lazy Tools")
 
-st.write("A tiny toolbox for tired researchers doing spatial biology and marker-related.
-It handles the boring parts of experiments and other repetitive tasks so that you can focus on the fun part.")
+st.write("""A tiny toolbox for tired researchers doing spatial biology and marker-related.
+It handles the boring parts of experiments and other repetitive tasks so that you can focus on the fun part.""")
 
 st.markdown("""
 ### Available tools
